2071dd871a4e9664b331a046676f6d5de5471f3b
RajKumar Sharma <130243990+Nobita41@users.noreply.github.com>
2024-08-23 12:41:57 +0530

    Add files via upload

---
+++ patterns/Question6.java
@@ -0,0 +1,14 @@
+package com.patterns;
+
+public class Question6 {
+ public static void main(String[] args) {
+	 int n=5;
+     for(int i=0;i<=n; i++){
+         
+         for(char j=(char) ('E' - i); j<='E'; j++){
+             System.out.print(j+ " ");
+         }
+         System.out.println();
+     }
+}
+}
---
+++ patterns/Question7.java
@@ -0,0 +1,40 @@
+package com.patterns;
+
+public class Question7 {
+	public static void main(String[] args) {
+		int n = 5;
+		int space = 0;
+		for (int i = 0; i <= n; i++) {
+
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print("*");
+			}
+			for (int j = 1; j <= space; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print("*");
+			}
+			space += 2;
+			System.out.println();
+
+		}
+		space = 8;
+		for (int i = 1; i <= n; i++) {
+
+			for (int j = 1; j <=  i; j++) {
+				System.out.print("*");
+			}
+			for (int j = 1; j <=space; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=  i; j++) {
+				System.out.print("*");
+			}
+			space -= 2;
+			System.out.println();
+
+		}
+
+	}
+}
